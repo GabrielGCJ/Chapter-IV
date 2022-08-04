149078114819ccdab53c1e1cb73f889156841d33
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-08-04 01:11:40 -0300

    Adição de testes para criação de cars

--- rentx/src/Modules/Cars/Repositories/ICarsRepository.ts
+++ rentx/src/Modules/Cars/Repositories/ICarsRepository.ts
@@ -1,10 +1,12 @@
 import { ICreateCarDTO } from "../dtos/ICreateCarDTO"
+import { Car } from "../Infra/typeorm/Entities/Car"
 
 
 
 interface ICarsReposytory {
 
-    create(data: ICreateCarDTO): Promise <void>
+    create(data: ICreateCarDTO): Promise <Car>
+    findByLicensePlate(license_plate: string):Promise<Car>
 }
 
 export { ICarsReposytory }
--- rentx/src/Modules/Cars/useCases/createCar/CreateCarUseCase.ts
+++ rentx/src/Modules/Cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,5 +1,8 @@
-import { ICarsReposytory } from "@modules/Cars/Repositories/ICarsRepository";
 import { inject, injectable } from "tsyringe";
+
+import { ICarsReposytory } from "@modules/Cars/Repositories/ICarsRepository";
+import { Car } from "@modules/Cars/Infra/typeorm/Entities/Car";
+import { AppError } from "@shared/Errors/AppError";
 
 
 interface IRequest {
@@ -28,8 +31,15 @@ class CreateCarUseCase {
         brand,
         category_id
     
-    }:IRequest): Promise<void>{
-        this.carsRepository.create({
+    }:IRequest): Promise<Car>{
+
+        const carAlreadyExist = await this.carsRepository.findByLicensePlate(license_plate);
+
+        if(carAlreadyExist){
+            throw new AppError("Car already exist")
+        }
+
+        const car = await this.carsRepository.create({
             name,
             description,
             daily_rate,
@@ -37,7 +47,9 @@ class CreateCarUseCase {
             fine_amount,
             brand,
             category_id
-        })                
+        })     
+        
+        return car
     }
 }
 
