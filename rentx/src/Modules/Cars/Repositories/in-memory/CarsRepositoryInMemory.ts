cb04d9bd7e7a9e420b884c5fef51f554b0c001ec
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-08-12 16:36:25 -0300

    Adição de testes para SpecificationCars

--- rentx/src/Modules/Cars/Repositories/in-memory/CarsRepositoryInMemory.ts
+++ rentx/src/Modules/Cars/Repositories/in-memory/CarsRepositoryInMemory.ts
@@ -4,8 +4,8 @@ import { ICarsReposytory } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsReposytory {
     cars: Car[] = [];
-    
-    async create({brand, category_id,daily_rate,description,fine_amount,name,license_plate}: ICreateCarDTO): Promise<Car> {
+
+    async create({ brand, category_id, daily_rate, description, fine_amount, name, license_plate }: ICreateCarDTO): Promise<Car> {
         const car = new Car()
 
         Object.assign(car, {
@@ -23,27 +23,31 @@ class CarsRepositoryInMemory implements ICarsReposytory {
         return car
     }
 
-    async findByLicensePlate(license_plate:string): Promise<any>{
+    async findByLicensePlate(license_plate: string): Promise<any> {
         return this.cars.find((car) => car.license_plate === license_plate)
     }
 
-    async findAvailable (
+    async findAvailable(
         brand?: string,
         category_id?: string,
         name?: string
-    ): Promise<Car[]> {        
+    ): Promise<Car[]> {
         const all = this.cars.filter((car) => {
             if (
                 car.available === true ||
-                ( brand && car.brand === brand ) ||
-                ( category_id && car.category_id === category_id ) ||
-                ( name && car.name === name )              
-        ) {
-            return car
-        }
-        return null
-    })
-    return all
+                (brand && car.brand === brand) ||
+                (category_id && car.category_id === category_id) ||
+                (name && car.name === name)
+            ) {
+                return car
+            }
+            return null
+        })
+        return all
+    }
+
+    async findById(id:string): Promise<any> {
+        return this.cars.find((car) => car.id === id)
     }
 }
 
