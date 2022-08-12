081c226df7682b44d0f0af72b3341cbc1d635ccf
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-08-12 09:20:52 -0300

    Adição de recursos de busca ao endpoint listAvailableCars

--- rentx/src/Modules/Cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ rentx/src/Modules/Cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,5 +1,6 @@
 import { ICarsReposytory } from "@modules/Cars/Repositories/ICarsRepository";
 import { Car } from "@modules/Cars/Infra/typeorm/Entities/Car";
+import { inject, injectable } from "tsyringe";
 
 
 interface IRequest {
@@ -10,15 +11,20 @@ interface IRequest {
 
 
 
-
-class ListCarsUseCase {
+@injectable()
+class ListAvailableCarsUseCase {
     constructor(
+        @inject("CarsRepository")
         private carsRepository: ICarsReposytory
     ){}
 
     async execute({category_id, brand, name}: IRequest):Promise<Car[]> {
 
-        const cars = await this.carsRepository.findAvailable()
+        const cars = await this.carsRepository.findAvailable(
+            brand,
+            category_id,
+            name
+        )
 
         return cars
 
@@ -27,4 +33,4 @@ class ListCarsUseCase {
 
 }
 
-export { ListCarsUseCase }
+export { ListAvailableCarsUseCase }
